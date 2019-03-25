e8d027d64b6b5fd7820ed1755200ae47a8fdb4b6
Jeonghoon Lee <1896357@johnabbottcollege.net>
2019-03-25 10:06:56 -0400

    update index.php page to link other pages.

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 </head>
 <body>
   <h1>Library HOME ^^</h1>
-  <a href="src/manage_book.php">register a book</a>
+  <a href="src/manage_book.php">register a book</a><br/>
+  <a href="templates/advance_search.html">Advanced Search</a><br/>
+  <a href="templates/booksDetails.html">book details</a><br/>
+  <a href="templates/main.html">main</a><br/>
+  <a href="templates/search_result.html">search result</a>
 </body>
 </html>
