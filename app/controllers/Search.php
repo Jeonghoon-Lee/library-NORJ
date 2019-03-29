6b183cf0d9ac76d97a54ee949c619125ca5c8485
Jeonghoon-Lee <hoon22.lee@gmail.com>
2019-03-29 00:40:54 -0400

    modify search page UI

--- app/controllers/Search.php
+++ app/controllers/Search.php
@@ -3,22 +3,33 @@
   class Search extends Controller {
     // define Model variable
     private $books;
+    private $languages;
+    private $categories;
 
     function __construct() {
       parent::__construct();
       // initialize Models
       $this->books = new BookModel($this->db); 
+      $this->languages = new LangModel($this->db); 
+      $this->categories = new CategoryModel($this->db); 
     }
 
     function search_book($f3) {
-      echo $f3->get('twig')->render('search.html');
+      $render_option = array(
+        'languages' => $this->languages->fetch_all(),
+        'categories' => $this->categories->fetch_all()
+      );
+      echo $f3->get('twig')->render('search.html', $render_option);
     }
 
     function search_result($f3) {
-      $search_option = array(
-        'books' => $this->books->fetch_all()
-      );
-      echo $f3->get('twig')->render('search_result.html', $search_option);
+      echo '<pre>';
+      echo print_r($f3->get('GET'));
+      echo '</pre>';
+      // $search_option = array(
+      //   'books' => $this->books->fetch_all()
+      // );
+      // echo $f3->get('twig')->render('search_result.html', $search_option);
     }
 
     function get_detail($f3) {
